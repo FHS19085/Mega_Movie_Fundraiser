694884757b6f1cf43d8335c996301eb75d743a7f
Perfect  Failure <106130232+FHS19085@users.noreply.github.com>
2022-05-25 23:13:33 +0000

    Added comments to remind

---
+++ main.py
@@ -0,0 +1,26 @@
+# Functions here
+
+# Main
+
+# Lists needed to hold data
+
+# Ask if user has used before and show instructions
+
+# Loop for ticket details
+
+# Get name
+
+# Get age
+
+# Calculate ticket price
+
+# Loop for snacks
+
+# Snack price
+
+# Payment method
+
+# Sales and profit
+
+# Output data to text file
+
